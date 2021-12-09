3f32da40ee1c939de1e5679da53f1cf1273f1177
Alex Paciolla <94958111+AlexPaciolla@users.noreply.github.com>
2021-12-09 10:52:36 +0100

    Add files via upload

--- tris 3.0.cpp
+++ tris 3.0.cpp
@@ -28,229 +28,237 @@ void titolo()
 
 
 int main(){
-	char toPrint[3][3];
-	char realOne[3][3];
-	char init='a';
-	char symbol='X';
-	char input;
-	char num_players;
-	char player;
-	bool reinput;
-	bool winner;
-	int match=1;
+	while(true)
+	{
+		char toPrint[3][3];
+		char realOne[3][3];
+		char init='a';
+		char symbol='X';
+		char input;
+		char num_players;
+		char player;
+		bool reinput;
+		bool winner;
+		int match=1;
+		
+		srand(time(NULL));
 	
-	srand(time(NULL));
-
-	//INIZIALIZZAZIONE MAPPA DA STAMPARE CON SPAZI VUOTI
-	for(int y=0; y<3; y++){
-		for(int x=0; x<3; x++){
-			toPrint[x][y]=' ';
-		}
-	}
-
-	//INIZIALIZAZZIONE DELLA MAPPA REALE CON LETTERE MINUSCOLE
-	for(int y=0; y<3; y++){
-		for(int x=0; x<3; x++){
-			realOne[x][y]=init;
-			init++;
-		}
-	}
-
-	//output del titolo
-	titolo();
-
-	//RICHIESTA DI GIOCARE CONTRO IL COMPUTER O IN PvP
-	do
-	{
-		printf("\nSeleziona il numero di giocatori [1 / 2]: ");
-		scanf(" %c",&num_players);
-
-		if(num_players!='1'&&num_players!='2')
-		{
-			printf("I giocatori possono essere solo 1 o 2!\n");
-		}
-	}
-	while(num_players!='1'&&num_players!='2');
-
-	if(num_players=='1')//SE SI GIOCA CONTRO IL COMPUTER
-	{
-		//SCELTA DEL SIMBOLO CON CUI SI VUOLE GIOCARE
-		//La X inizia per prima in qualsiasi partita
-		do
-		{
-			printf("Vuoi giocare come giocatore X o come giocatore O? ");	//si decide che simbolo si vuole usare, il giocatore X inizia sempre per primo
-			scanf(" %c",&player);
-
-			if(player!='X'&&player!='O')
-			{
-				printf("Il simbolo puo' essere solo X oppure O\n");			//se il simbolo è diverso da X o da O si da un feedback all'utente
+		//INIZIALIZZAZIONE MAPPA DA STAMPARE CON SPAZI VUOTI
+		for(int y=0; y<3; y++){
+			for(int x=0; x<3; x++){
+				toPrint[x][y]=' ';
 			}
 		}
-		while(player!='X'&&player!='O');			//si consente il reinserimento
-		//IL SIMBOLO è STATO SCELTO
-	}
-
-	//PULIZIA SCHERMO
-	system("cls");
-
-	//INIZIO DEL GAME LOOP
-	while(match<=9)
-	{
-		//STAMPO IL TITOLO
+	
+		//INIZIALIZAZZIONE DELLA MAPPA REALE CON LETTERE MINUSCOLE
+		for(int y=0; y<3; y++){
+			for(int x=0; x<3; x++){
+				realOne[x][y]=init;
+				init++;
+			}
+		}
+	
+		//output del titolo
 		titolo();
-
-		//STAMPA DELLA MAPPA CAMPIONE
-		for(int y=0; y<3; y++){
-
-			printf("	");//il seguente printf contiene elementi "decorativi"
-
-			for(int x=0; x<3; x++){
-				printf(" %c ",realOne[x][y]);
-
-				if(x!=2){//il seguente if contiene elementi "decorativi"
-					printf("|");
-				}
-			}
-
-			if(y!=2){//il seguente if contiene elementi "decorativi"
-				printf("\n	-----------\n");
-			}
-		}
-
-		printf("\n\n");
-
-		//STAMPA DELLA MAPPA ATTUALE
-		for(int y=0; y<3; y++){
-
-			printf("	");//il seguente printf contiene elementi "decorativi"
-
-			for(int x=0; x<3; x++){
-				printf(" %c ",toPrint[x][y]);
-
-				if(x!=2){//il seguente if contiene elementi "decorativi"
-					printf("|");
-				}
-			}
-
-			if(y!=2){//il seguente if contiene elementi "decorativi"
-				printf("\n	-----------\n");
-			}
-		}
-
-
-		//I TURNI SI ALTERNANO
-		if(match%2==0){
-			symbol='O';
-		}else{
-			symbol='X';
-		}
-
+	
+		//RICHIESTA DI GIOCARE CONTRO IL COMPUTER O IN PvP
 		do
 		{
-			if(num_players=='2'||(num_players=='1'&&((player=='X'&&match%2==1)||(player=='O'&&match%2==0)))){		//elenco dei casi in cui il giocatore deve inserire la casella
-				//INPUT DEL GIOCATORE
-				do
-				{
-					printf("\n\nE' il turno del giocatore %c.\nDove vuoi mettere il tuo simbolo? ", symbol);
-					scanf(" %c",&input);
-
-					if(input>'h'||input<'a')
-					{
-						printf("La casella inserita non esiste!");
-					}
-				}while(input>'h'||input<'a');
+			printf("\nSeleziona il numero di giocatori [1 / 2]: ");
+			scanf(" %c",&num_players);
+	
+			if(num_players!='1'&&num_players!='2')
+			{
+				printf("I giocatori possono essere solo 1 o 2!\n");
 			}
-			else																									//il computer deve inserire la sua casella
-			{                                                      
-				input=(rand()%9)+'a';                                                                               //al momento la scelta della casella è casuale
-			}                                                                                                       //la selezione intelligente arriverà in futuro
-
-                                                                                                                               
-			//COMPILAZIONE DELLA CASELLA
-			for(int y=0; y<3; y++){
-				for(int x=0; x<3; x++){
-					if(realOne[x][y]==input&&toPrint[x][y]==' '){
-						toPrint[x][y]=symbol;
-					}else{
-						reinput=true;                                                                              	//#BUG!!! REINPUT RISULTA SEMPRE VERO DOPO IL PRIMO TURNO
-					}                                                                                               //STO ATTUALMENTE CERCANDO DI RISOLVERE
+		}
+		while(num_players!='1'&&num_players!='2');
+	
+		if(num_players=='1')//SE SI GIOCA CONTRO IL COMPUTER
+		{
+			//SCELTA DEL SIMBOLO CON CUI SI VUOLE GIOCARE
+			//La X inizia per prima in qualsiasi partita
+			do
+			{
+				printf("Vuoi giocare come giocatore X o come giocatore O? ");	//si decide che simbolo si vuole usare, il giocatore X inizia sempre per primo
+				scanf(" %c",&symbol);
+	
+				if(symbol!='X'&&symbol!='O')
+				{
+					printf("Il simbolo puo' essere solo X oppure O\n");			//se il simbolo è diverso da X o da O si da un feedback all'utente
 				}
 			}
-
-			//FEEDBACK IN CASO DI CASELLA OCCUPATA
-			if(reinput==true&&(num_players=='2'||(num_players=='1'&&((player=='X'&&match%2==1)||(player=='O'&&match%2==0))))){
-				printf("La casella selezionata e' gia' occupata!");
-			}
-
-		}while(reinput==true);
-
+			while(symbol!='X'&&symbol!='O');			//si consente il reinserimento
+			//IL SIMBOLO è STATO SCELTO
+		}
+	
 		//PULIZIA SCHERMO
 		system("cls");
-
-		//AGGIORNAMENTO DEL TURNO
-		match++;
-
-		//CONTROLLO SE CI SONO VINCITORI
-		//controllo le righe
-		for(int y=0;y<3;y++){
-			int x=0;
-
-			if(toPrint[x][y]==toPrint[x+1][y]&&toPrint[x+1][y]==toPrint[x+2][y]&&toPrint[x+2][y]!=' '){
+	
+		//INIZIO DEL GAME LOOP
+		while(match<=9)
+		{
+			//STAMPO IL TITOLO
+			titolo();
+	
+			//STAMPA DELLA MAPPA CAMPIONE
+			for(int y=0; y<3; y++){
+	
+				printf("	");//il seguente printf contiene elementi "decorativi"
+	
+				for(int x=0; x<3; x++){
+					printf(" %c ",realOne[x][y]);
+	
+					if(x!=2){//il seguente if contiene elementi "decorativi"
+						printf("|");
+					}
+				}
+	
+				if(y!=2){//il seguente if contiene elementi "decorativi"
+					printf("\n	-----------\n");
+				}
+			}
+	
+			printf("\n\n");
+	
+			//STAMPA DELLA MAPPA ATTUALE
+			for(int y=0; y<3; y++){
+	
+				printf("	");//il seguente printf contiene elementi "decorativi"
+	
+				for(int x=0; x<3; x++){
+					printf(" %c ",toPrint[x][y]);
+	
+					if(x!=2){//il seguente if contiene elementi "decorativi"
+						printf("|");
+					}
+				}
+	
+				if(y!=2){//il seguente if contiene elementi "decorativi"
+					printf("\n	-----------\n");
+				}
+			}
+	
+	
+			//I TURNI SI ALTERNANO
+			if(match%2==1){
+				player='X';
+			}else{
+				player='O';
+			}
+	
+			do
+			{
+				if(num_players=='2'||(num_players=='1'&&((symbol=='X'&&player=='X')||(symbol=='O'&&player=='O')))){		//elenco dei casi in cui il giocatore deve inserire la casella
+					//INPUT DEL GIOCATORE
+					do
+					{
+						printf("\n\nE' il turno del giocatore %c.\nDove vuoi mettere il tuo simbolo? ", player);
+						scanf(" %c",&input);
+	
+						if(input>'i'||input<'a')
+						{
+							printf("La casella inserita non esiste!");
+						}
+					}while(input>'i'||input<'a');
+				}
+				else if(num_players=='1'&&((symbol=='X'&&player=='O')||(symbol=='O'&&player=='X'))){		            //il computer deve inserire la sua casella
+					input=(rand()%9)+'a';                                                                               //al momento la scelta della casella è casuale
+				}                                                                                                       //la selezione intelligente arriverà in futuro
+	
+	                                                                                                                               
+				//COMPILAZIONE DELLA CASELLA
+				for(int y=0; y<3; y++){
+					for(int x=0; x<3; x++){
+						if(realOne[x][y]==input&&toPrint[x][y]==' '){
+							reinput=false;
+							toPrint[x][y]=player;
+						}else if(realOne[x][y]==input&&toPrint[x][y]!=' '){
+							reinput=true;
+							
+							//FEEDBACK IN CASO DI CASELLA OCCUPATA
+							if(num_players=='2'||(num_players=='1'&&((symbol=='X'&&match%2==1)||(symbol=='O'&&match%2==0)))){
+								printf("La casella selezionata e' gia' occupata!");
+							}                                                                             
+						}                                                                                               
+					}
+				}
+	
+			}while(reinput==true);
+	
+			//PULIZIA SCHERMO
+			system("cls");
+	
+			//AGGIORNAMENTO DEL TURNO
+			match++;
+	
+			//CONTROLLO SE CI SONO VINCITORI
+			//controllo le righe
+			for(int y=0;y<3;y++){
+				int x=0;
+	
+				if(toPrint[x][y]==toPrint[x+1][y]&&toPrint[x+1][y]==toPrint[x+2][y]&&toPrint[x+2][y]!=' '){
+					match+=9; 
+					winner=true;
+					printf("\n\nIl giocatore %c ha vinto!\n\n", player);
+				}
+			}
+	
+			//controllo le colonne
+			for(int x=0;x<3;x++){
+				int y=0;
+	
+				if(toPrint[x][y]==toPrint[x][y+1]&&toPrint[x][y+1]==toPrint[x][y+2]&&toPrint[x][y+2]!=' '){
+					match+=9;
+					winner=true;
+					printf("\n\nIl giocatore %c ha vinto!\n\n", player);
+				}
+			}
+	
+			//controllo la diagonale "aei"
+			int x=0; int y=0;
+			if(toPrint[x][y]==toPrint[x+1][y+1]&&toPrint[x][y]==toPrint[x+2][y+2]&&toPrint[x][y]!=' '){
+				match+=9; 
+				winner=true;
+				printf("Il giocatore %c ha vinto!\n\n", player);
+			}
+			
+			//controllo la diagonale "ceg"
+			x=2; y=2;
+			if(toPrint[x][y]==toPrint[x-1][y-1]&&toPrint[x][y]==toPrint[x-2][y-2]&&toPrint[x][y]!=' '){
 				match+=9;
 				winner=true;
-				printf("\n\nIl giocatore %c ha vinto!\n\n", symbol);
+				printf("Il giocatore %c ha vinto!\n\n", player);			
+			}
+	
+			//in caso di pareggio
+			if(match>9&&winner==false){
+				printf("Abbiamo un pareggio!\n\n");
 			}
 		}
-
-		//controllo le colonne
-		for(int x=0;x<3;x++){
-			int y=0;
-
-			if(toPrint[x][y]==toPrint[x][y+1]&&toPrint[x][y+1]==toPrint[x][y+2]&&toPrint[x][y+2]!=' '){
-				match+=9;
-				winner=true;
-				printf("\n\nIl giocatore %c ha vinto!\n\n", symbol);
+	
+	
+		//STAMPA DELLA MAPPA ATTUALE
+		for(int y=0; y<3; y++){
+	
+			printf("	");//il seguente printf contiene elementi "decorativi"
+	
+			for(int x=0; x<3; x++){
+				printf(" %c ",toPrint[x][y]);
+	
+				if(x!=2){//il seguente if contiene elementi "decorativi"
+					printf("|");
+				}
 			}
-		}
-
-		//controllo la diagonale "aei"
-		int x=0; int y=0;
-		if(toPrint[x][y]==toPrint[x+1][y+1]&&toPrint[x][y]==toPrint[x+2][y+2]&&toPrint[x][y]!=' '){
-			match+=9;
-			winner=true;
-			printf("Il giocatore %c ha vinto!\n\n", symbol);
+	
+			if(y!=2){//il seguente if contiene elementi "decorativi"
+				printf("\n	-----------\n");
+			}
 		}
 		
-		//controllo la diagonale "ceg"
-		x=2; y=2;
-		if(toPrint[x][y]==toPrint[x-1][y-1]&&toPrint[x][y]==toPrint[x-2][y-2]&&toPrint[x][y]!=' '){
-			match+=9;
-			winner=true;
-			printf("Il giocatore %c ha vinto!\n\n", symbol);			
-		}
-
-		//in caso di pareggio
-		if(match==9&&winner==false){
-			printf("Abbiamo un pareggio!");
-		}
+		//Azioni finali
+		printf("\n\n");
+		system("pause");
+		system("cls");
 	}
-
-	//STAMPA DELLA MAPPA ATTUALE
-	for(int y=0; y<3; y++){
-
-		printf("	");//il seguente printf contiene elementi "decorativi"
-
-		for(int x=0; x<3; x++){
-			printf(" %c ",toPrint[x][y]);
-
-			if(x!=2){//il seguente if contiene elementi "decorativi"
-				printf("|");
-			}
-		}
-
-		if(y!=2){//il seguente if contiene elementi "decorativi"
-			printf("\n	-----------\n");
-		}
-	}
-
 }
